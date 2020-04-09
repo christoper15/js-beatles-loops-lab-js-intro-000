fb305bd0e43dd962d31aea099649a41ee052cc7a
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-09 19:30:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var array = [];
-
 function theBeatlesPlay(musicians, instruments) {
+  var array = [];
   for ( let i = 0 ; i< musicians.length ; i++  ) {
       array[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   return array;
+}
+
+function johnLennonFacts(array) {
+
+
+  
 }
