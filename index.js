28394bef9f4f361133c1894bf30b4c188d90541b
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-09 19:27:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+var array = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for ( let i = 0 ; i< musicians.length() ; i++  ) {
+      array[i] = ` ${musicians[i]} plays ${instruments[i]}.`
+  }
+  return array;
+}
